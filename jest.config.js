dbea2942e11d59fc4bb7398d0d4534b9853fea65
Khalid-lazrag <khalid.lazrag@etu.uae.ac.ma>
2025-09-02 17:01:01 +0100

    chore: add Jest configuration and testing libraries
    - Updated package.json to include Jest, testing-library, and TypeScript support.
    - Created jest.config.js for Jest configuration with jsdom environment and module name mapping.
    - Added jest.setup.js to import custom jest matchers from @testing-library/jest-dom.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/lib/(.*)$': '<rootDir>/src/lib/$1',
+  },
+  transform: {
+    '^.+\.(ts|tsx)$': 'ts-jest',
+  },
+};
